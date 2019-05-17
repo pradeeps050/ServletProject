c09ad66cafb9774bb45d16ef59b28f65e6e0558c
Pradeep Sharma <Pradeep Sharma@Pradeep>
2019-05-17 22:58:24 +0530

    First Maven Project.

---
+++ ExpServlet/src/com/ps/demo/FServlet.java
@@ -0,0 +1,33 @@
+package com.ps.demo;
+
+import java.io.IOException;
+import javax.servlet.GenericServlet;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * Servlet implementation class FServlet
+ */
+@WebServlet("/FServlet")
+public class FServlet extends GenericServlet implements Servlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see GenericServlet#GenericServlet()
+     */
+    public FServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#service(ServletRequest request, ServletResponse response)
+	 */
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		response.getWriter().print("Hello.........");
+	}
+
+}
